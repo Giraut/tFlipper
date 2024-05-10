9c0836c5971ee482f5b0ce96176cc193129a4570
PCo <>
2024-05-10 11:20:38 +0300

    Added -H command line switch to render the Flipper Zero's display in high-density semigraphics, which don't respect the display's aspect ration but fit in a 80x24 console.

--- tFlipper.py
+++ tFlipper.py
@@ -67,50 +67,303 @@ keyboard_to_flipper_input = {
   "B":           "LONG BACK"
 }
 
-keyboard_to_flipper_help1 = [
-"_______________________________________________________________",
-"|                  SHORT PRESS                                 ",
-"|              ___________________                             ",
-"|              |       UP:       |                             ",
-"|              |     w, ^, k     |                             ",
-"|              +-----------------+                             ",
-"| ___________  ___________________  ___________                ",
-"| |  LEFT:  |  |       OK:       |  |  RIGHT: |                ",
-"| | <, a, h |  | SPACE, ENTER, o |  | d, l, > |                ",
-"| +---------+  +-----------------+  +---------+                ",
-"|              ___________________       _____________________ ",
-"|              |      DOWN:      |       |       BACK:       | ",
-"|              |     s, v, j     |       | BACKSPACE, DEL, b | ",
-"|              +-----------------+       +-------------------+ ",
-"+--------------------------------------------------------------"]
-
-keyboard_to_flipper_help2 = [
-"________________________________________",
-"            LONG PRESS                 |",
-"            __________                 |",
-"            |   UP:  |                 |",
-"            |  W, K  |                 |",
-"            +--------+                 |",
-"__________  __________  __________     |",
-"|  LEFT: |  |   OK:  |  | RIGHT: |     |",
-"|  A, H  |  |    O   |  |  D, L  |     |",
-"+--------+  +--------+  +--------+     |",
-"            __________       _________ |",
-"            |  DOWN: |       | BACK: | |",
-"            |  S, J  |       |   B   | |",
-"            +--------+       +-------+ |",
-"---------------------------------------+"]
+keyboard_to_flipper_help = [
+"________________________________________________________________",
+"|                     _________________                        |",
+"|                     |      UP:      |                        |",
+"|                     |               |                        |",
+"|              Long > |    [W] [K]    |                        |",
+"|             Short > |     w ^ k     |                        |",
+"|                     +---------------+                        |",
+"|        ___________  _________________  ___________           |",
+"|        |  LEFT:  |  |      OK:      |  |  RIGHT: |           |",
+"|        |         |  |               |  |         |           |",
+"| Long > | [A] [H] |  |      [O]      |  | [D] [L] |           |",
+"|Short > |  < a h  |  | SPACE ENTER o |  |  d l >  |           |",
+"|        +---------+  +---------------+  +---------+           |",
+"|                     _________________         ______________ |",
+"|                     |     DOWN:     |         |    BACK:   | |",
+"|                     |               |         |            | |",
+"|              Long > |    [S] [V]    |  Long > |     [B]    | |",
+"|             Short > |     s v j     | Short > | BACK DEL b | |",
+"|                     +---------------+         +------------+ |",
+"+--------------------------------------------------------------+"]
 
 
 
 ## defines
 #
 
-unicode_block_elements = [
-  "\u2588",	# Full block
-  "\u2584",	# Lower half block
-  "\u2580",	# Upper half block
-  " ",		# Space
+unicode_blocks_1x2 = [
+  "\u2588",	# Full block:		#
+		#			#
+
+  "\u2584",	# Lower half block:	.
+		#			#
+
+  "\u2580",	# Upper half block:	#
+		#			.
+
+  " ",		# Space:		.
+		#			.
+]
+
+unicode_blocks_2x3 = [
+  "\u2588",	# Full block:		##
+		#			##
+		#			##
+
+  "\U0001fb3b",	#			.#
+		#			##
+		#			##
+
+  "\U0001fb38",	#			##
+		#			.#
+		#			##
+
+  "\U0001fb37",	#			.#
+		#			.#
+		#			##
+
+  "\U0001fb2c",	#			##
+		#			##
+		#			.#
+
+  "\U0001fb2b",	#			.#
+		#			##
+		#			.#
+
+  "\U0001fb28",	#			##
+		#			.#
+		#			.#
+
+  "\u2590",	# Right half block:	.#
+		#			.#
+		#			.#
+
+  "\U0001fb3a",	#			#.
+		#			##
+		#			##
+
+  "\U0001fb39",	#			..
+		#			##
+		#			##
+
+  "\U0001fb36",	#			#.
+		#			.#
+		#			##
+
+  "\U0001fb35",	#			..
+		#			.#
+		#			##
+
+  "\U0001fb2a",	#			#.
+		#			##
+		#			.#
+
+  "\U0001fb29",	#			..
+		#			##
+		#			.#
+
+  "\U0001fb27",	#			#.
+		#			.#
+		#			.#
+
+  "\U0001fb26",	#			..
+		#			.#
+		#			.#
+
+  "\U0001fb34",	#			##
+		#			#.
+		#			##
+
+  "\U0001fb33",	#			.#
+		#			#.
+		#			##
+
+  "\U0001fb30",	#			##
+		#			..
+		#			##
+
+  "\U0001fb2f",	#			.#
+		#			..
+		#			##
+
+  "\U0001fb25",	#			##
+		#			#.
+		#			.#
+
+  "\U0001fb24",	#			.#
+		#			#.
+		#			.#
+
+  "\U0001fb21",	#			##
+		#			..
+		#			.#
+
+  "\U0001fb20",	#			.#
+		#			..
+		#			.#
+
+  "\U0001fb32",	#			#.
+		#			#.
+		#			##
+
+  "\U0001fb31",	#			..
+		#			#.
+		#			##
+
+  "\U0001fb2e",	#			#.
+		#			..
+		#			##
+
+  "\U0001fb2d",	#			..
+		#			..
+		#			##
+
+  "\U0001fb23",	#			#.
+		#			#.
+		#			.#
+
+  "\U0001fb22",	#			..
+		#			#.
+		#			.#
+
+  "\U0001fb1f",	#			#.
+		#			..
+		#			.#
+
+  "\U0001fb1e",	#			..
+		#			..
+		#			.#
+
+  "\U0001fb1d",	#			##
+		#			##
+		#			#.
+
+  "\U0001fb1c",	#			.#
+		#			##
+		#			#.
+
+  "\U0001fb19",	#			##
+		#			.#
+		#			#.
+
+  "\U0001fb18",	#			.#
+		#			.#
+		#			#.
+
+  "\U0001fb0e",	#			##
+		#			##
+		#			..
+
+  "\U0001fb0d",	#			.#
+		#			##
+		#			..
+
+  "\U0001fb0a",	#			##
+		#			.#
+		#			..
+
+  "\U0001fb09",	#			.#
+		#			.#
+		#			..
+
+  "\U0001fb1b",	#			#.
+		#			##
+		#			#.
+
+  "\U0001fb1a",	#			..
+		#			##
+		#			#.
+
+  "\U0001fb17",	#			#.
+		#			.#
+		#			#.
+
+  "\U0001fb16",	#			..
+		#			.#
+		#			#.
+
+  "\U0001fb0c",	#			#.
+		#			##
+		#			..
+
+  "\U0001fb0b",	#			..
+		#			##
+		#			..
+
+  "\U0001fb08",	#			#.
+		#			.#
+		#			..
+
+  "\U0001fb07",	#			..
+		#			.#
+		#			..
+
+  "\U0001fb15",	#			##
+		#			#.
+		#			#.
+
+  "\U0001fb14",	#			.#
+		#			#.
+		#			#.
+
+  "\U0001fb12",	#			##
+		#			..
+		#			#.
+
+  "\U0001fb11",	#			.#
+		#			..
+		#			#.
+
+  "\U0001fb06",	#			##
+		#			#.
+		#			..
+
+  "\U0001fb05",	#			.#
+		#			#.
+		#			..
+
+  "\U0001fb02",	#			##
+		#			..
+		#			..
+
+  "\U0001fb01",	#			.#
+		#			..
+		#			..
+
+  "\u258c",	# Left half block:	#.
+		#			#.
+		#			#.
+
+  "\U0001fb13",	#			..
+		#			#.
+		#			#.
+
+  "\U0001fb10",	#			#.
+		#			..
+		#			#.
+
+  "\U0001fb0f",	#			..
+		#			..
+		#			#.
+
+  "\U0001fb04",	#			#.
+		#			#.
+		#			..
+
+  "\U0001fb03",	#			..
+		#			#.
+		#			..
+
+  "\U0001fb00",	#			#.
+		#			..
+		#			..
+
+  " ",		# Space:		..
+		#			..
+		#			..
 ]
 
 # Special characters
@@ -188,36 +441,47 @@ def main():
 	  type = str
 	)
 
+  argparser.add_argument(
+	  "-H", "--high-density-semigraphics",
+	  help = "Use 2x3 unicode block characters for rendering (smaller "
+			"console needed but incorrect aspect ratio). Default: "
+			"1x2 blocks",
+	  action = "store_true"
+	)
+
   args = argparser.parse_args()
 
-  # Combine and semigraphic-ize the keymap help strings
-  keymap_help = []
-  for l1, l2 in zip(keyboard_to_flipper_help1, keyboard_to_flipper_help2):
-    l = (" " + l1 + l2 + " ").\
-		replace(" _", " \u250c").replace("_ ", "\u2510 ").\
-		replace("_", "\u2500").replace("|", "\u2502").\
-		replace(" +", " \u2514").replace("+ ", "\u2518 ").\
-		replace("-", "\u2500").replace("<", "\u2190").\
-		replace("^", "\u2191").replace(">", "\u2192").\
-		replace("v", "\u2193")[1:-1]
-    keymap_help.append(l)
+  # Semigraphic-ize the keymap help strings
+  keymap_help = [(" " + l + " ").\
+			replace(" _", " \u250c").replace("_ ", "\u2510 ").\
+			replace("_", "\u2500").replace("|", "\u2502").\
+			replace(" +", " \u2514").replace("+ ", "\u2518 ").\
+			replace("-", "\u2500").replace("<", "\u2190").\
+			replace("^", "\u2191").replace(">", "\u2192").\
+			replace("v", "\u2193")[1:-1]
+			for l in keyboard_to_flipper_help]
+
+  width = 64 if args.high_density_semigraphics else 128
+  height = 22 if args.high_density_semigraphics else 32
+
+  nb_display_lines = height + 2
 
   keymap_help_line_len = len(keymap_help[0])
-  keymap_help_overlay_at_col = int((128 - keymap_help_line_len) / 2)
-  keymap_help_overlay_at_line = int((32 - len(keymap_help)) / 2)
+  keymap_help_overlay_at_col = int((width - keymap_help_line_len) / 2)
+  keymap_help_overlay_at_line = int((height - len(keymap_help)) / 2)
 
   # Connect to the Flipper Zero
   p = FlipperProto(serial_port = args.device)
 
   # Get the Flipper Zero's name
   flipper_name = "[ " + p.device_info["hardware_name"] + " ]"
-  flipper_name_spacing = " " * int((128 - len(flipper_name)) / 2)
+  flipper_name_spc1 = " " * int((width - len(flipper_name)) / 2)
+  flipper_name_spc2 = " " * (width - len(flipper_name_spc1) - len(flipper_name))
 
   # Bottom help line
   bottom_line = "[ Ctrl-K to show/hide keymap ]     [ Ctrl-C to stop ]"
-  bottom_line_spacing = " " * int((128 - len(bottom_line)) / 2)
-
-  nb_display_lines = 34
+  bottom_line_spc1 = " " * int((width - len(bottom_line)) / 2)
+  bottom_line_spc2 = " " * (width - len(bottom_line_spc1) - len(bottom_line))
 
   # ANSI sequences to home the cursor and send it to the end of the display
   home_cursor = x_lines_up.format(nb_display_lines)
@@ -240,9 +504,25 @@ def main():
       assert len(screen_data) == 1024
 
       # Turn the screen data into lines of unicode block elements
-      imglines = ["".join([unicode_block_elements[(screen_data[i] >> j) & 0b11]
-			for i in range(k, k + 128)])
-		for k in range(0, 1024, 128) for j in range(0, 8, 2)]
+      if args.high_density_semigraphics:
+
+        imglines = []
+        for k in range(0, 1024, 384):
+
+          c = [screen_data[i] + (screen_data[i + 128] << 8) + \
+		((screen_data[i + 256] << 16) if i < 768 else 0xff0000) \
+		for i in range(k, k + 128)]
+
+          for j in (0, 3, 6, 9, 12, 15) + ((18, 21) if k < 768 else ()):
+            imglines.append("".join([unicode_blocks_2x3[
+						((c[i] >> j) & 7) | \
+						(((c[i + 1] >> j) & 7) << 3)] \
+					for i in range(0, 128, 2)]))
+
+      else:
+        imglines = ["".join([unicode_blocks_1x2[(screen_data[i] >> j) & 0b11]
+				for i in range(k, k + 128)])
+			for k in range(0, 1024, 128) for j in (0, 2, 4, 6)]
 
       # If the keymap help should be displayed, overlay it over the lines
       if show_keymap:
@@ -257,17 +537,17 @@ def main():
 				[keymap_help_overlay_at_col + \
 					keymap_help_line_len:]
 
-      # Create the final ASCII art
-      aa =  flipper_name_spacing + flipper_name + CR + LF + \
+      # Print the final ASCII art
+      print(flipper_name_spc1 + flipper_name + flipper_name_spc2 + CR + LF + \
 		"".join([set_bg_color.format(ansi_8bit_black) + \
 				set_fg_color.format(ansi_8bit_orange) + \
 				l + attribute_reset + CR + LF \
 				for l in imglines]) + \
-		bottom_line_spacing + bottom_line + CR + \
-		home_cursor
+		bottom_line_spc1 + bottom_line + bottom_line_spc2 + CR + \
+		home_cursor)
 
-      # Print the ASCII art on the console
-      print(aa)
+      # Flush stdout to make sure the console is updated immediately
+      sys.stdout.flush()
 
       # Process messages from the input thread
       while True:
